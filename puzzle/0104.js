9c607c55518ed152db7da05d469bdaf60e7ad83f
William Zhou <zwlbnu@gmail.com>
2016-01-04 13:57:40 +0800

    edit puzzle

--- puzzle/0104.js
+++ puzzle/0104.js
@@ -1,10 +1,8 @@
-window.onload = function() {
   var chooseLevel = document.getElementById('chooseLevel');
   var chooseLevelButton = document.getElementById('chooseLevelButton');
   chooseLevelButton.onclick = function() {
     var cookieString = 'gs=0 0 0 0 ';
-    cookieString += chooseLevel.innerHTML;
+    cookieString += chooseLevel.value;
     document.cookie = cookieString;
     location.replace(location.href);
   }
-}
