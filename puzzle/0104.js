010a722abaa939878c8908fedb61965d5ce70cbd
William Zhou <william@Lilium.local>
2017-06-16 21:25:59 +0800

    调整目录结构，删除无用项目

--- puzzle/0104.js
+++
@@ -1,8 +0,0 @@
-  var chooseLevel = document.getElementById('chooseLevel');
-  var chooseLevelButton = document.getElementById('chooseLevelButton');
-  chooseLevelButton.onclick = function() {
-    var cookieString = 'gs=0 0 0 0 ';
-    cookieString += chooseLevel.value;
-    document.cookie = cookieString;
-    location.replace(location.href);
-  }
