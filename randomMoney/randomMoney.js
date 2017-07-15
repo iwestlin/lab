1173539dd2dab64169faa1abf138caba1352e8ba
william <i@viegg.com>
2017-07-15 22:43:13 +0800

    解决财富值总数会增加的问题

--- randomMoney/randomMoney.js
+++ randomMoney/randomMoney.js
@@ -1,10 +1,7 @@
-/*
-  注意，有考虑不完善的地方，重复几千次后财富值总数可能会大于初始值，原因是财富值没有设置负数。
- */
-
-var n = 26
-var m = 100
-var a = Array(n).fill(m)
+var n = 26 // 人数
+var m = 100 // 初始财富值
+var t = 0 // 临时记录每一轮有多少人财富值本该变成负数
+var a = Array(n).fill(m) // 存储所有人财富值的数组
 // change(1000)
 // console.log(a.slice().sort((x,y) => x - y))
 // console.log(a.reduce((x,y) => x + y))
@@ -13,9 +10,10 @@ function change (times) {
   for (var count = 0; count < times; count++) {
     a = a.map(x => minusOne(x))
     // a = a.map(x => x - 1)
-    for (var i = 0; i < n; i++) {
+    for (var i = 0; i < n - t; i++) {
       a[randomRange(0, n)]++
     }
+    t = 0
   }
 }
 
@@ -27,6 +25,7 @@ function minusOne (x) {
   if (x > 0) {
     return x - 1
   } else {
+    t++
     return 0
   }
 }
