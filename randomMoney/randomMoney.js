e1550ab2b5495e84f8fef64b703d6916e2561c6d
william <i@viegg.com>
2017-07-15 23:12:20 +0800

    简化程序

--- randomMoney/randomMoney.js
+++ randomMoney/randomMoney.js
@@ -1,6 +1,5 @@
 var n = 26 // 人数
 var m = 100 // 初始财富值
-var t = 0 // 临时记录每一轮有多少人财富值本该变成负数
 var a = Array(n).fill(m) // 存储所有人财富值的数组
 // change(1000)
 // console.log(a.slice().sort((x,y) => x - y))
@@ -8,12 +7,11 @@ var a = Array(n).fill(m) // 存储所有人财富值的数组
 
 function change (times) {
   for (var count = 0; count < times; count++) {
-    a = a.map(x => minusOne(x))
-    // a = a.map(x => x - 1)
-    for (var i = 0; i < n - t; i++) {
-      a[randomRange(0, n)]++
-    }
-    t = 0
+    a.forEach((value, index) => giveAway(value, index))
+    // a = a.map(x => x - 1) // 允许财富值为负数
+    // for (var i = 0; i < n; i++) {
+    //   a[randomRange(0, n)]++
+    // }
   }
 }
 
@@ -21,11 +19,9 @@ function randomRange (min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
 }
 
-function minusOne (x) {
-  if (x > 0) {
-    return x - 1
-  } else {
-    t++
-    return 0
+function giveAway (value, index) {
+  if (value > 0) {
+    a[index]--
+    a[randomRange(0, n)]++
   }
 }
