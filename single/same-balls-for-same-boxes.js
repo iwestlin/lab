c7e2f9d25d232b2716312f3d8fba4e61fc6bffaf
william <i@viegg.com>
2017-07-29 18:08:28 +0800

    standard js style

--- single/same-balls-for-same-boxes.js
+++ single/same-balls-for-same-boxes.js
@@ -1,35 +1,35 @@
-function haha(n, m) {
-  var result = [];
+function haha (n, m) {
+  var result = []
 
-  function nm(balls, boxes, former, limits) {
+  function nm (balls, boxes, former, limits) {
     if (balls < 0 || boxes < 0) {
-      return;
+      return
     }
     if (balls === 0) {
       for (let i = 0; i < boxes; i++) {
-        former.push(0);
+        former.push(0)
       }
-      result.push(former);
-      return;
+      result.push(former)
+      return
     }
     if (boxes === 1) {
-      former.push(balls);
-      result.push(former);
-      return;
+      former.push(balls)
+      result.push(former)
+      return
     }
     for (let i = limits; i > 0; i--) {
-      if (i * boxes >= balls) { //避免出现重复的情况
-        //比如假设7放3已经记录了[4，2，1]这种情况，那么在下一轮循环放入了4个球和1个球到两个盒子里，还剩2个球，1个盒子，此时limit为1，limit*boxes = 1*1 = 1 < balls = 2，程序不再分配，直接返回。
-        var copy = former.slice();
-        copy.push(i);
-        nm(balls - i, boxes - 1, copy, i);
+      if (i * boxes >= balls) { // 避免出现重复的情况
+        // 比如假设7放3已经记录了[4，2，1]这种情况，那么在下一轮循环放入了4个球和1个球到两个盒子里，还剩2个球，1个盒子，此时limit为1，limit*boxes = 1*1 = 1 < balls = 2，程序不再分配，直接返回。
+        var copy = former.slice()
+        copy.push(i)
+        nm(balls - i, boxes - 1, copy, i)
       } else {
-        return;
+        return
       }
     }
   }
-  nm(n, m, [], n);
-  console.table(result);
-  return result;
+  nm(n, m, [], n)
+  console.table(result)
+  return result
 }
-haha(7, 3);
+haha(7, 3)
