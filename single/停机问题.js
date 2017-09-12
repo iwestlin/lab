8584b93b80bb20d193dd148c0716bd6a87f04662
william <i@viegg.com>
2017-09-12 23:20:59 +0800

    一点修正

--- single/停机问题.js
+++ single/停机问题.js
@@ -1,14 +1,12 @@
 function H (program, input) {} // 假想的万能程序，判定 program(input) 是否会死循环，如果会则输出 true，反之输出 false
 
-function paradox (input) {
+function paradox (input) { // 将 paradox 本身输入，将产生矛盾
   if (H(input, input)) {
-    return false
+    return // 如果 H(paradox, paradox) 为真，则直接返回，并不产生死循环，与 H 函数的定义相悖
   } else {
-    return true
+    while(true) {} // 如果 H(paradox, paradox) 为假，产生死循环，与 H 相悖
   }
 }
 
-H(paradox, paradox) // 产生矛盾，如果 H(paradox, paradox) 为真则输出假，如果假则输出真
-// = paradox(paradox)
-// = if H(paradox, paradox) return false
-//   else return true 
+H(paradox, paradox)
+// 判定 paradox(paradox) 是否会产生死循环
