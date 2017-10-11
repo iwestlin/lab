796d66a5bbdbf7c2f37e52a9e0815e7f63b64b3d
william <i@viegg.com>
2017-10-11 22:57:59 +0800

    添加 最长公共子序列 JavaScript 实现

---
+++ single/lcs.js
@@ -0,0 +1,26 @@
+var a = "1234helloworld2j3293gdgdg6464642093jdklwjflwewt423412897491274uhrf12rfr12"
+var b = "03424helloworldfdfcp043p92jflkf79hhedhey464w6hsry4648721fjbmcvszfq39wnf39804"
+console.log(lcs(a, b)) // 34helloworld329d6464642jf3994
+//               haskell: 24helloworld2j9d64646429wf384
+
+function lcs (a, b) {
+  function rec (m, n) {
+    var key = m + '-' + n
+    if (cache[key]) {
+      return cache[key]
+    }
+    if (m === 0 || n === 0) {
+      cache[key] = ''
+    } else if (a[m] === b[n]) {
+      cache[key] = rec(m - 1, n - 1) + a[m]
+    } else {
+      cache[key] = longer(rec(m - 1, n), rec(m, n - 1))
+    }
+    return cache[key]
+  }
+  return rec(a.length - 1, b.length - 1)
+}
+
+function longer (a, b) {
+  return a.length > b.length ? a : b
+}
